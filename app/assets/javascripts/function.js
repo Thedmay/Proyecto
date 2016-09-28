40ad912914617a5b1f97d6d166e16a3387a4665a
Diego <garcia.castillo23@gmail.com>
2016-09-28 11:51:37 -0300

    ultimo, creo

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -69,3 +69,46 @@ $(function validar_numero(field){
 		field.setAttribute("value", x);
 	}
 });
+
+$(function holamundo() {
+
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+	 if(dd<10){
+	        dd='0'+dd
+	    } 
+	    if(mm<10){
+	        mm='0'+mm
+	    } 
+
+	today = yyyy+'-'+mm+'-'+dd;
+
+	try{
+		document.getElementById("bill_fecha").setAttribute("max", today);
+	}catch(e){}
+
+	try{
+		document.getElementById("lincense_fechaInicio").setAttribute("max", today);
+	}catch(e){}
+
+	try{
+	document.getElementById("employee_fecha_ingreso").setAttribute("max", today);
+	}catch(e){}
+
+	try{
+	document.getElementById("fecha_fecha").setAttribute("min", today);
+	document.getElementById("fecha_fecha").setAttribute("max", today);
+	}catch(e){}
+	try{
+	document.getElementById("order_fecha").setAttribute("min", today);
+	document.getElementById("order_fecha").setAttribute("max", today);
+
+	document.getElementById("order_fecha_final").setAttribute("min", today);
+	yyyy = today.getFullYear()+1;
+	today = yyyy+'-'+mm+'-'+dd;
+	document.getElementById("order_fecha_final").setAttribute("max", today);
+	}catch(e){}
+
+});
